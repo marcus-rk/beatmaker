06cf47c9d078b7f8678e9febe4ea55cecc220586
marcus-rk <marcus.rappenborg@gmail.com>
2023-10-23 13:33:00 +0200

    made the sequencer array with row and button elements.

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 // makj0005
+
+
+const sequencerElement = document.querySelector('.sequencer');
+const rowElements = sequencerElement.querySelectorAll('.row')
+
+const sequencer = [];
+rowElements.forEach(rowElement => {
+    const row = {
+        name: rowElement.querySelector('span').innerText,
+        buttons: []
+    };
+
+    const buttonElements = rowElement.querySelectorAll('button')
+
+    buttonElements.forEach(buttonElement => {
+        const button = {
+            id : buttonElement.id,
+            isActive : false,
+        }
+
+        row.buttons.push(button);
+    });
+
+    sequencer.push(row);
+});
+
+console.log(sequencer);
