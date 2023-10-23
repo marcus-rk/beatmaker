e21c23e8853809828f7a7b13ab961bfaa312fa22
marcus-rk <marcus.rappenborg@gmail.com>
2023-10-23 15:03:24 +0200

    added correct colers and audio for rows.

--- main.js
+++ main.js
@@ -6,30 +6,48 @@ const rowElements = sequencerElement.querySelectorAll('.row')
 
 const sequencer = [];
 rowElements.forEach(rowElement => {
-    const row = {
-        name: rowElement.querySelector('span').innerText,
-        buttons: []
+    const spanElement = rowElement.querySelector('span');
+    const rowObject = {
+        name: spanElement.innerText,
+        buttons: [],
+        audioType: '',
+        colorPalette: '',
     };
+
+    // get audio type from row span name etc. kick, snare etc.
+    switch (rowObject.name) {
+        case 'Kick':
+            rowObject.audioType = 'kick/kick_1.wav';
+            rowObject.colorPalette = 'kick';
+            break;
+        case 'Snare':
+            rowObject.audioType = 'snare/snare_1.wav';
+            rowObject.colorPalette = 'snare';
+            break;
+    }
+
+    spanElement.classList.add(rowObject.colorPalette);
 
     const buttonElements = rowElement.querySelectorAll('button')
 
     buttonElements.forEach(buttonElement => {
-        const button = {
-            id : buttonElement.id,
-            isActive : false,
-            audio : new Audio();
+        const buttonObject = {
+            id: buttonElement.id, // TODO: is this needed?
+            isActive: false,
+            audio: new Audio(`/audio/${rowObject.audioType}`),
+            colorPalette: rowObject.colorPalette,
         }
 
+        // Change status of buttonObject and buttonElement
         buttonElement.addEventListener('click', () => {
-            // Toggle the isActive property when the button is clicked
-            button.isActive = !button.isActive;
-            // You can add further logic here to visually update the button's appearance
+            buttonObject.isActive = !buttonObject.isActive;
+
+            buttonElement.classList.toggle('active' , buttonObject.isActive);
+            buttonElement.classList.toggle(rowObject.colorPalette, buttonObject.isActive);
         });
 
-        row.buttons.push(button);
+        rowObject.buttons.push(buttonObject);
     });
 
-    sequencer.push(row);
+    sequencer.push(rowObject);
 });
-
-console.log(sequencer);
